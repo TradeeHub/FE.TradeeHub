d0b6600572ddbf8b8bb3b262ae9d8427526f6a63
Marenglen Doci <tradeehub@gmail.com>
2023-12-28 09:56:15 +0000

    - testing upstream

--- app/dashboard/appointments/page.tsx
+++ app/dashboard/appointments/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 const Appointments = () => {
-  return <div style={{ height: 400, width: "100%" }}>Appointments</div>;
+  return <div style={{ height: 400, width: "100%" }}>Appointments Calendar</div>;
 };
 
 export default Appointments;
