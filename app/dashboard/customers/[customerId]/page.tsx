aa94bdae8e8deced7f0f761b46ea4976e5ba4258
Glen Doci <tradeehub@gmail.com>
2024-01-04 23:12:47 +0000

    - temp

--- app/dashboard/customers/[customerId]/page.tsx
+++ app/dashboard/customers/[customerId]/page.tsx
@@ -9,7 +9,7 @@ import { HiOutlineCalendarDays } from 'react-icons/hi2';
 import { CiTimer } from 'react-icons/ci';
 import { FaBatteryEmpty } from 'react-icons/fa6';
 import { PiHouseLineLight } from 'react-icons/pi';
-import { BsHouses } from "react-icons/bs";
+import { BsHouses } from 'react-icons/bs';
 
 const Customer = ({ params }: { params: { customerId: string } }) => {
   const { data, loading } = useCustomer(params.customerId);
@@ -23,7 +23,18 @@ const Customer = ({ params }: { params: { customerId: string } }) => {
   const mainPhone = customer?.phoneNumbers[0].phoneNumber;
   const recentProperty = customer?.properties[0]?.propertyAddress.fullAddress;
   const iconClass = 'h-6 w-5 text-brand-accent1';
-  const textClass =  'text-sm leading-6 text-brand-secondary1d';
+  const textClass = 'text-sm leading-6 text-brand-secondary1d';
+  const tabs = [
+    { name: 'Quotes', href: '#', current: true },
+    { name: 'Jobs', href: '#', current: false },
+    { name: 'Invoices', href: '#', current: false },
+    { name: 'Appointments', href: '#', current: false },
+    { name: 'Activity', href: '#', current: false },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ');
+  }
   return (
     <>
       <Suspense fallback={loading}>
@@ -53,10 +64,7 @@ const Customer = ({ params }: { params: { customerId: string } }) => {
                 </div>
                 <div className='mt-2 flex w-full flex-none gap-x-4 px-6'>
                   <dt className='text-sm'>
-                    <FaBatteryEmpty
-                      className={iconClass}
-                      aria-hidden='true'
-                    />
+                    <FaBatteryEmpty className={iconClass} aria-hidden='true' />
                   </dt>
                   <dd className='rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-blue-600 ring-1 ring-inset ring-blue-600/20'>
                     Lead
@@ -64,10 +72,7 @@ const Customer = ({ params }: { params: { customerId: string } }) => {
                 </div>
                 <div className='mt-2 flex w-full flex-none gap-x-4 px-6'>
                   <dt className='text-sm'>
-                    <HiOutlinePhone
-                      className={iconClass}
-                      aria-hidden='true'
-                    />
+                    <HiOutlinePhone className={iconClass} aria-hidden='true' />
                   </dt>
                   <dd className={textClass}>
                     <span>{mainPhone}</span>
@@ -88,10 +93,7 @@ const Customer = ({ params }: { params: { customerId: string } }) => {
 
                 <div className='mt-2 flex w-full flex-none gap-x-4 px-6'>
                   <dt>
-                    <AiOutlineMail
-                      className={iconClass}
-                      aria-hidden='true'
-                    />
+                    <AiOutlineMail className={iconClass} aria-hidden='true' />
                   </dt>
                   <dd className={textClass}>
                     <span>{customer?.emails[0].email}</span>
@@ -113,10 +115,7 @@ const Customer = ({ params }: { params: { customerId: string } }) => {
                 </div>
                 <div className='mt-2 flex w-full flex-none items-center gap-x-4 px-6 pb-3'>
                   <dt className='flex-none text-sm'>
-                    <CiTimer
-                      className={iconClass}
-                      aria-hidden='true'
-                    />
+                    <CiTimer className={iconClass} aria-hidden='true' />
                   </dt>
                   <dd className={textClass}>
                     <time dateTime={modifiedAtFormatted}>
@@ -138,25 +137,78 @@ const Customer = ({ params }: { params: { customerId: string } }) => {
                   </dt>
                   <dd className='text-md font-semibold leading-6 text-gray-900'>
                     <h3 className='text-base font-semibold leading-6 text-brand-secondary1d'>
-                       <span>Properties</span>
+                      <span>Properties</span>
                     </h3>
                   </dd>
                 </div>
-                  {customer?.properties?.map(property => (
-                    <div key={property?.id} className='mt-2 flex w-full flex-wrap gap-x-4 px-6'>
-                      <dt>
-                        <PiHouseLineLight
-                          className={iconClass}
-                          aria-hidden='true'
-                        />
-                      </dt>
-                      <dd className={textClass}>
-                        <span>{property?.propertyAddress?.fullAddress}</span>
-                      </dd>
-                    </div>
-                  ))}
+                {customer?.properties?.map((property) => (
+                  <div
+                    key={property?.id}
+                    className='mt-2 flex w-full flex-wrap gap-x-4 px-6'
+                  >
+                    <dt>
+                      <PiHouseLineLight
+                        className={iconClass}
+                        aria-hidden='true'
+                      />
+                    </dt>
+                    <dd className={textClass}>
+                      <span>{property?.propertyAddress?.fullAddress}</span>
+                    </dd>
+                  </div>
+                ))}
               </dl>
             </div>
+          </div>
+        </div>
+
+        <div>
+          <div className='sm:hidden'>
+            <label htmlFor='tabs' className='sr-only'>
+              Select a tab
+            </label>
+            {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+            <select
+              id='tabs'
+              name='tabs'
+              className='block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500'
+              defaultValue={tabs.find((tab) => tab.current).name}
+            >
+              {tabs.map((tab) => (
+                <option key={tab.name}>{tab.name}</option>
+              ))}
+            </select>
+          </div>
+          <div className='hidden sm:block'>
+            <nav
+              className='isolate flex divide-x divide-gray-200 rounded-lg shadow'
+              aria-label='Tabs'
+            >
+              {tabs.map((tab, tabIdx) => (
+                <a
+                  key={tab.name}
+                  href={tab.href}
+                  className={classNames(
+                    tab.current
+                      ? 'text-gray-900'
+                      : 'text-gray-500 hover:text-gray-700',
+                    tabIdx === 0 ? 'rounded-l-lg' : '',
+                    tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
+                    'group relative min-w-0 flex-1 overflow-hidden bg-white px-4 py-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10',
+                  )}
+                  aria-current={tab.current ? 'page' : undefined}
+                >
+                  <span>{tab.name}</span>
+                  <span
+                    aria-hidden='true'
+                    className={classNames(
+                      tab.current ? 'bg-indigo-500' : 'bg-transparent',
+                      'absolute inset-x-0 bottom-0 h-0.5',
+                    )}
+                  />
+                </a>
+              ))}
+            </nav>
           </div>
         </div>
       </Suspense>
