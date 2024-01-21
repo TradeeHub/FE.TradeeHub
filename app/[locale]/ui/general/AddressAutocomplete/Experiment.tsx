f01686a5b8dfe4e4683ac1f56b47af735b6e17ca
Marenglen Doci <tradeehub@gmail.com>
2024-01-21 21:48:16 +0000

    - fixed my focus issues

--- app/[locale]/ui/general/AddressAutocomplete/Experiment.tsx
+++ app/[locale]/ui/general/AddressAutocomplete/Experiment.tsx
@@ -14,9 +14,9 @@ import {
   PopoverTrigger,
 } from '@radix-ui/react-popover';
 
-type AddressAutocompleteProps = { 
+type AddressAutocompleteProps = {
   onPlaceSelected: (place: google.maps.places.PlaceResult) => void;
-}
+};
 
 type AutocompletePrediction = {
   description: string;
@@ -26,6 +26,7 @@ type AutocompletePrediction = {
 const AddressAutocomplete: React.FC<AddressAutocompleteProps> = ({
   onPlaceSelected,
 }) => {
+  const isMountedRef = useRef(false);
   const [userLocation, setUserLocation] = useState<{
     latitude: number;
     longitude: number;
@@ -35,10 +36,10 @@ const AddressAutocomplete: React.FC<AddressAutocompleteProps> = ({
   const [suggestions, setSuggestions] = useState<AutocompletePrediction[]>([]);
   const autocompleteServiceRef =
     useRef<google.maps.places.AutocompleteService | null>(null);
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY;
   const [isPopoverOpen, setIsPopoverOpen] = useState<boolean>(false);
-const popoverContentRef = useRef<HTMLDivElement>(null);
-
+  const popoverContentRef = useRef<HTMLDivElement>(null);
+  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_PLACE_API_KEY;
+  const hasMadeSelection = useRef<boolean>(false);
   const fetchPredictions = debounce((input) => {
     console.log(
       'Fetching predictions for:',
@@ -76,36 +77,7 @@ const popoverContentRef = useRef<HTMLDivElement>(null);
     }
   }, 700);
 
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: apiKey ?? '',
-      libraries: ['places'],
-    });
-
-    loader.load().then(() => {
-      autocompleteServiceRef.current =
-        new google.maps.places.AutocompleteService();
-    });
-    console.log('API key:', apiKey);
-  }, [apiKey]);
-
-  useEffect(() => {
-    fetchPredictions(inputValue);
-    if(suggestions.length > 0) {
-      setIsPopoverOpen(true);
-    }
-  }, [inputValue]);
-
-  useEffect(() => {
-    // If there are suggestions, open the popover and focus the input field
-    if (suggestions.length > 0) {
-      setIsPopoverOpen(true);
-      inputRef.current?.focus();
-    }
-  }, [suggestions]);
-
-    const handleSelect = (location: AutocompletePrediction) => {
-
+  const handleSelect = (location: AutocompletePrediction) => {
     setInputValue(location.description);
 
     const placesService = new google.maps.places.PlacesService(
@@ -120,6 +92,7 @@ const popoverContentRef = useRef<HTMLDivElement>(null);
           if (status === google.maps.places.PlacesServiceStatus.OK && place) {
             onPlaceSelected(place);
             setSuggestions([]);
+            console.log('hasMadeSelection 2222:', hasMadeSelection.current);
             console.log('Place details:', place, suggestions);
           } else {
             console.error('Error getting details:', status);
@@ -131,53 +104,89 @@ const popoverContentRef = useRef<HTMLDivElement>(null);
     }
   };
 
-  console.log('Current suggestions:', suggestions); // Check the state right before rendering
+  useEffect(() => {
+    console.log('hasMadeSelection:', hasMadeSelection.current);
+    if (isMountedRef.current && !hasMadeSelection.current) {
+      fetchPredictions(inputValue);
+      if (suggestions.length > 0) {
+        console.log('Opening popover...');
+        setIsPopoverOpen(true);
+      }
+    }else {
+      hasMadeSelection.current = false;
+    }
+  }, [inputValue]);
 
-return (
-  <>
-    <div className='flex items-center'>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Command>
-            <CommandInput
-              ref={inputRef}
-              placeholder='Type an address...'
-              value={inputValue}
-              onValueChange={setInputValue}
-            />
-          </Command>
-        </PopoverTrigger>
-        {isPopoverOpen && (
-          <PopoverContent
-            ref={popoverContentRef}
-            className='z-50 bg-white p-1 opacity-100 shadow-md'
-            side='bottom'
-            align='start'
-            // Add this to prevent the popover content from capturing focus on open
-            onFocus={(event) => event.preventDefault()}
-          >
+  useEffect(() => {
+    // If there are suggestions, open the popover and focus the input field
+    console.log('Suggestions:', suggestions);
+    if (suggestions.length > 0 && isMountedRef.current) {
+      console.log('Opening popover... 1111')
+      setIsPopoverOpen(true);
+      console.log('isPopoverOpen:', isPopoverOpen);
+      inputRef.current?.focus();
+    }
+  }, [suggestions]);
+
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: apiKey ?? '',
+      libraries: ['places'],
+    });
+
+    loader.load().then(() => {
+      autocompleteServiceRef.current =
+        new google.maps.places.AutocompleteService();
+    });
+    isMountedRef.current = true;
+  }, []);
+
+  return (
+    <>
+      <div className='flex items-center'>
+        <Popover>
+          <PopoverTrigger onClick={(e) => e.preventDefault()} asChild>
             <Command>
-              <CommandList>
-                <CommandGroup>
-                  {suggestions.map((suggestion) => (
-                    <CommandItem
-                      key={suggestion.place_id}
-                      onSelect={() => {
-                        handleSelect(suggestion);
-                      }}
-                    >
-                      {suggestion.description}
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-              </CommandList>
+              <CommandInput
+                ref={inputRef}
+                placeholder='Type an address...'
+                value={inputValue}
+                onValueChange={setInputValue}
+              />
             </Command>
-          </PopoverContent>
-        )}
-      </Popover>
-    </div>
-  </>
-);
+          </PopoverTrigger>
+          {isPopoverOpen && (
+            <PopoverContent
+              ref={popoverContentRef}
+              className='z-50 bg-white p-1 opacity-100 shadow-md'
+              side='bottom'
+              align='start'
+              onOpenAutoFocus={(e) => e.preventDefault()}
+              forceMount={suggestions.length > 0 ? true : undefined}
+              >
+              <Command>
+                <CommandList>
+                  <CommandGroup>
+                    {suggestions.map((suggestion) => (
+                      <CommandItem
+                        key={suggestion.place_id}
+                        onSelect={() => {
+                          handleSelect(suggestion);
+                          hasMadeSelection.current = true;
+                        }}
+                      >
+                        {suggestion.description}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          )}
+        </Popover>
+      </div>
+    </>
+  );
 };
 
 export default AddressAutocomplete;
