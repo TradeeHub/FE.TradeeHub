e07336f049016958e9458504dd6cb92fadf07714
Marenglen Doci <tradeehub@gmail.com>
2024-02-15 20:32:17 +0000

    - moved emails into it's on multi email component

--- app/[locale]/components/AddCustomerModal/AddCustomerModal.tsx
+++ app/[locale]/components/AddCustomerModal/AddCustomerModal.tsx
@@ -51,6 +51,7 @@ import { RootState } from '@/lib/store';
 import { useSelector } from 'react-redux';
 import ProgressBar from '../../ui/auth/ProgressBar/ProgressBar';
 import MultiPhoneNumber from './AddPhoneNumber/AddPhoneNumber';
+import MultiEmail from './MultiEmail/MultiEmail';
 
 type ModalProps = {
   isOpen: boolean;
@@ -336,18 +337,6 @@ const AddCustomerModal: React.FC<ModalProps> = ({
     addNewCustomer(customerData);
   };
 
-  const addPhoneNumber = () => {
-    appendPhone({
-      phoneNumber: '',
-      phoneNumberType: '',
-      receiveNotifications: true,
-    });
-  };
-
-  const addEmail = () => {
-    appendEmail({ email: '', emailType: '', receiveNotifications: true });
-  };
-
   const onPlaceSelectForProperty = (place: UserPlace | null) => {
     if (place) {
       setValue('property', place);
@@ -583,96 +572,9 @@ const AddCustomerModal: React.FC<ModalProps> = ({
               <MultiPhoneNumber
                 form={form as UseFormReturn<AddCustomerFormRequest>}
               />
-
-              {/* Email Addresses */}
-              {emailFields.map((field, index) => (
-                <div key={field.id} className='flex items-center'>
-                  {' '}
-                  {/* Bottom margin for spacing between rows */}
-                  <div className='w-1/4 pr-2'>
-                    {' '}
-                    {/* Assign width to 1/4 of container and padding to the right */}
-                    <FormField
-                      control={form.control}
-                      name={`emails.${index}.emailType`}
-                      render={({ field }) => (
-                        <SelectWithInputForm<
-                          AddCustomerFormRequest,
-                          `emails.${typeof index}.emailType`
-                        >
-                          form={form as UseFormReturn<AddCustomerFormRequest>}
-                          field={
-                            field as ControllerRenderProps<
-                              AddCustomerFormRequest,
-                              `emails.${typeof index}.emailType`
-                            >
-                          }
-                          options={emailTypeOptions}
-                          inputPlaceHolder='Email Type'
-                          defaultValue='Personal'
-                        />
-                      )}
-                    />
-                  </div>
-                  <div className='flex-1 px-2'>
-                    {' '}
-                    {/* Flex grow and padding on both sides */}
-                    <FormField
-                      control={form.control}
-                      name={`emails.${index}.email`}
-                      render={({ field }) => (
-                        <AuthInputWithIcon
-                          field={field}
-                          placeholder='Email Address'
-                          type='email'
-                        />
-                      )}
-                    />
-                  </div>
-                  <div className='w-20 px-2'>
-                    {' '}
-                    {/* Assign fixed width for the switch container and padding */}
-                    <FormField
-                      control={form.control}
-                      name={`emails.${index}.receiveNotifications`}
-                      render={({ field }) => (
-                        <SwitchWithLabel
-                          checked={field.value}
-                          onCheckedChange={field.onChange}
-                          onLabel='ON'
-                          offLabel='OFF'
-                          aria-label='Notifications'
-                          label='Notifications'
-                        />
-                      )}
-                    />
-                  </div>
-                  <div className='w-12'>
-                    {' '}
-                    {/* Assign fixed width for the button container */}
-                    {index !== 0 ? (
-                      <Button
-                        type='button'
-                        variant={'ghost'}
-                        onClick={() => removeEmail(index)}
-                        className='remove-button'
-                        size='icon'
-                      >
-                        <RxCross2 />
-                      </Button>
-                    ) : (
-                      <CustomButton
-                        type='button'
-                        variant='ghost'
-                        size='sm'
-                        onClick={addEmail}
-                      >
-                        Add
-                      </CustomButton>
-                    )}
-                  </div>
-                </div>
-              ))}
+              <MultiEmail
+                form={form as UseFormReturn<AddCustomerFormRequest>}
+              />
 
               {/* Property*/}
               <div className='pt-2'>
---
+++ app/[locale]/components/AddCustomerModal/MultiEmail/MultiEmail.tsx
@@ -0,0 +1,136 @@
+// MultiEmail.jsx or MultiEmail.tsx if you're using TypeScript
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { RxCross2 } from 'react-icons/rx';
+import {
+  ControllerRenderProps,
+  UseFormReturn,
+  useFieldArray,
+} from 'react-hook-form';
+import { AddCustomerFormRequest } from '@/app/[locale]/types/sharedTypes';
+import { FormField } from '@/components/ui/form';
+import SelectWithInputForm from '../../SelectWithInputForm/SelectWithInputForm';
+import { AuthInputWithIcon } from '@/app/[locale]/ui/auth/AuthInputWithIcon/AuthInputWithIcon';
+import { CustomButton } from '../../CustomButton/CustomButton';
+import { SwitchWithLabel } from '../../SwitchWithLabel/SwitchWithLabel';
+
+const emailTypeOptions = [
+  { label: 'Personal', value: 'Personal' },
+  { label: 'Work', value: 'Work' },
+  { label: 'Other', value: 'Other' },
+];
+
+type MultiEmailProps = {
+  form: UseFormReturn<AddCustomerFormRequest>;
+};
+
+const MultiEmail: React.FC<MultiEmailProps> = ({ form }) => {
+  const { control } = form;
+  const {
+    fields: emailFields,
+    append: appendEmail,
+    remove: removeEmail,
+  } = useFieldArray({
+    control,
+    name: 'emails',
+  });
+
+  const addEmail = () => {
+    appendEmail({
+      email: '',
+      emailType: '',
+      receiveNotifications: true,
+    });
+  };
+
+  return (
+    <>
+      {emailFields.map((field, index) => (
+        <div key={field.id} className='flex items-center'>
+          <div className='w-1/4 pr-2'>
+            <FormField
+              control={form.control}
+              name={`emails.${index}.emailType`}
+              render={({ field }) => (
+                <SelectWithInputForm<
+                  AddCustomerFormRequest,
+                  `emails.${typeof index}.emailType`
+                >
+                  form={form as UseFormReturn<AddCustomerFormRequest>}
+                  field={
+                    field as ControllerRenderProps<
+                      AddCustomerFormRequest,
+                      `emails.${typeof index}.emailType`
+                    >
+                  }
+                  options={emailTypeOptions}
+                  inputPlaceHolder='Email Type'
+                  defaultValue='Personal'
+                />
+              )}
+            />
+          </div>
+          <div className='flex-1 px-2'>
+            {' '}
+            {/* Flex grow and padding on both sides */}
+            <FormField
+              control={form.control}
+              name={`emails.${index}.email`}
+              render={({ field }) => (
+                <AuthInputWithIcon
+                  field={field}
+                  placeholder='Email Address'
+                  type='email'
+                />
+              )}
+            />
+          </div>
+          <div className='w-20 px-2'>
+            {' '}
+            {/* Assign fixed width for the switch container and padding */}
+            <FormField
+              control={form.control}
+              name={`emails.${index}.receiveNotifications`}
+              render={({ field }) => (
+                <SwitchWithLabel
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                  onLabel='ON'
+                  offLabel='OFF'
+                  aria-label='Notifications'
+                  label='Notifications'
+                />
+              )}
+            />
+          </div>
+          <div className='w-12'>
+            {' '}
+            {/* Assign fixed width for the button container */}
+            {index !== 0 ? (
+              <Button
+                type='button'
+                variant={'ghost'}
+                onClick={() => removeEmail(index)}
+                className='remove-button'
+                size='icon'
+              >
+                <RxCross2 />
+              </Button>
+            ) : (
+              <CustomButton
+                type='button'
+                variant='ghost'
+                size='sm'
+                onClick={addEmail}
+              >
+                Add
+              </CustomButton>
+            )}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MultiEmail;
