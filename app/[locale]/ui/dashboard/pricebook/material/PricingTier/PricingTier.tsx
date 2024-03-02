019ddaa656cf0db22adce775af0c9809a4091644
Marenglen Doci <tradeehub@gmail.com>
2024-03-02 20:08:53 +0000

    - added number  restriction only

--- app/[locale]/ui/dashboard/pricebook/material/PricingTier/PricingTier.tsx
+++ app/[locale]/ui/dashboard/pricebook/material/PricingTier/PricingTier.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   ArrayPath,
   Controller,
@@ -14,14 +14,13 @@ import {
 import { FaXmark } from 'react-icons/fa6';
 import { MdAdd } from 'react-icons/md';
 import { FormControl, FormItem } from '@/components/ui/form';
-import { PricingTierEntity } from '@/generatedGraphql';
 
 type SelectWithInputFormProps<
-  PricingTierEntityWithId extends FieldValues,
-  TFieldName extends FieldPath<PricingTierEntityWithId>,
+  TFieldValues extends FieldValues,
+  TFieldName extends FieldPath<TFieldValues>,
 > = {
-  form: UseFormReturn<PricingTierEntityWithId>;
-  field: ControllerRenderProps<PricingTierEntityWithId, TFieldName>;
+  form: UseFormReturn<TFieldValues>;
+  field: ControllerRenderProps<TFieldValues, TFieldName>;
   title?: string;
   currencySymbol?: string;
 };
@@ -66,6 +65,19 @@ const PricingTier = <
     }
   };
 
+  const removeAndUpdate = (index: number) => {
+    // If not the last item, update the next item's 'Range From' before removing.
+    if (index < fields.length - 1) {
+      const previousField = fields[index - 1] as TFieldValues;
+      const previousRangeTo = index > 0 ? previousField.unitRange.max : 0;
+      setValue(
+        `${field.name}.${index + 1}.unitRange.min` as Path<TFieldValues>,
+        (previousRangeTo + 1) as PathValue<TFieldValues, Path<TFieldValues>>,
+      );
+    }
+    remove(index);
+  };
+
   const handleBlur = (index: number) => {
     const currentRangeTo = watch(
       `${field.name}.${index}.unitRange.max` as Path<TFieldValues>,
@@ -95,7 +107,7 @@ const PricingTier = <
   const addNewTier = () => {
     const lastTierIndex = fields.length - 1;
 
-    const lastTier = fields[lastTierIndex] as TFieldValues; // Update the type to PricingTierEntity
+    const lastTier = fields[lastTierIndex] as TFieldValues;
     const minValue = lastTier ? Number(lastTier.unitRange?.max) + 1 : 0;
     append({
       cost: 0,
@@ -112,151 +124,95 @@ const PricingTier = <
     }
   };
 
-  return (<>
-    <span className='pl-3 text-md text-primary font-semibold pb-0 font-roboto'>{title}</span>
-    <div className='overflow-hidden rounded-lg border border-gray-200 bg-white shadow'>
-      <table className='min-w-full divide-y divide-gray-200'>
-        <thead className='bg-gray-50'>
-          <tr>
-            <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
-              Range From
-            </th>
-            <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
-              Range To
-            </th>
-            <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
-              Cost
-            </th>
-            <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
-              Price
-            </th>
-<th className='text-center text-sm font-semibold uppercase tracking-wider'>
-  <button
-    type='button'
-    onClick={addNewTier}
-    className='mx-auto py-1 text-green-700 hover:text-green-900 focus:outline-none'
-    style={{ fontSize: '1.2rem', display: 'block' }}
-  >
-    <MdAdd style={{ fontSize: '1.2rem' }} />
-  </button>
-</th>
-
-
-          </tr>
-        </thead>
-        <tbody className='divide-y divide-gray-200 bg-white'>
-          {fields.map((field, index) => (
-            <tr key={field.id}>
-              <td className='whitespace-nowrap px-2 py-2'>
-                <FormItem>
-                  <FormControl>
-                    <Controller
-                      name={
-                        `pricingTiers.${index}.unitRange.min` as Path<TFieldValues>
-                      }
-                      control={control}
-                      render={({ field }) => (
-                        <input
-                          {...field}
-                          onChange={(e) =>
-                            field.onChange(Number(e.target.value))
-                          }
-                          type='number'
-                          className='form-input mx-auto mt-1 block w-20 rounded-none border-b border-gray-300 px-2 py-1 text-center text-gray-400 focus:outline-none focus:ring-0 sm:text-sm'
-                          readOnly
-                        />
-                      )}
-                    />
-                  </FormControl>
-                </FormItem>
-              </td>
-
-              <td className='whitespace-nowrap px-2 py-2'>
-                <FormItem>
-                  <FormControl>
-                    <Controller
-                      name={
-                        `pricingTiers.${index}.unitRange.max` as Path<TFieldValues>
-                      }
-                      control={control}
-                      render={({ field }) => (
-                        <input
-                          {...field}
-                          type='number'
-                          className='form-input mx-auto mt-1 block w-20 rounded-none border-b border-gray-300 px-2 py-1 text-center focus:border-indigo-500 focus:outline-none focus:ring-0 sm:text-sm'
-                          onChange={(e) => field.onChange(e.target.value)} // Allow any change
-                          onBlur={() => handleBlur(index)} // Apply validation on blur
-                        />
-                      )}
-                    />
-                  </FormControl>
-                </FormItem>
-              </td>
-
-              <td className='whitespace-nowrap px-2 py-2'>
-                <FormItem>
-                  <FormControl>
-                    <Controller
-                      control={control}
-                      name={`pricingTiers.${index}.cost` as Path<TFieldValues>}
-                      render={({
-                        field: { ref, value, onChange, ...restField },
-                      }) => (
-                        <div className='relative'>
+  return (
+    <>
+      <span className='text-md pb-0 pl-3 font-roboto font-semibold text-primary'>
+        {title}
+      </span>
+      <div className='overflow-hidden rounded-lg border border-gray-200 bg-white shadow'>
+        <table className='min-w-full divide-y divide-gray-200'>
+          <thead className='bg-gray-50'>
+            <tr>
+              <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
+                Range From
+              </th>
+              <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
+                Range To
+              </th>
+              <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
+                Cost
+              </th>
+              <th className='px-2 py-3 text-center text-xs font-medium uppercase tracking-wider text-gray-500'>
+                Price
+              </th>
+              <th className='text-center text-sm font-semibold uppercase tracking-wider'>
+                <button
+                  type='button'
+                  onClick={addNewTier}
+                  className='mx-auto py-1 text-green-700 hover:text-green-900 focus:outline-none'
+                  style={{ fontSize: '1.2rem', display: 'block' }}
+                >
+                  <MdAdd style={{ fontSize: '1.2rem' }} />
+                </button>
+              </th>
+            </tr>
+          </thead>
+          <tbody className='divide-y divide-gray-200 bg-white'>
+            {fields.map((field, index) => (
+              <tr key={field.id}>
+                <td className='whitespace-nowrap px-2 py-2'>
+                  <FormItem>
+                    <FormControl>
+                      <Controller
+                        name={
+                          `pricingTiers.${index}.unitRange.min` as Path<TFieldValues>
+                        }
+                        control={control}
+                        render={({ field }) => (
                           <input
-                            {...restField}
-                            ref={ref}
-                            value={`${currencySymbol}${value}`}
-                            onChange={(e) => {
-                              // Ensure we strip out the currency symbol before updating the value
-                              const newValue = e.target.value.replace(
-                                currencySymbol ?? '',
-                                '',
-                              );
-                              onChange(newValue);
-                            }}
-                            className='form-input mt-1 block w-full rounded-none border-b border-gray-300 py-1 pl-4 text-center focus:border-indigo-500 focus:outline-none focus:ring-0 sm:text-sm'
-                            style={{
-                              paddingLeft: `${currencySymbol?.length ?? '' + 1}ch`,
-                            }}
+                            {...field}
+                            onChange={(e) =>
+                              field.onChange(Number(e.target.value))
+                            }
+                            type='number'
+                            className='form-input mx-auto mt-1 block w-20 rounded-none border-b border-gray-300 px-2 py-1 text-center text-gray-400 focus:outline-none focus:ring-0 sm:text-sm'
+                            readOnly
                           />
-                        </div>
-                      )}
-                    />
-                  </FormControl>
-                </FormItem>
-              </td>
+                        )}
+                      />
+                    </FormControl>
+                  </FormItem>
+                </td>
 
-              <td className='relative whitespace-nowrap px-2 py-2'>
-                <FormItem>
-                  <FormControl>
-                    <>
-                      {/* Absolute positioning for Margin and Profit */}
-                      <div className='absolute -top-1 right-1 flex flex-col items-end'>
-                        <span className='mt-1 text-xs text-blue-500'>
-                          {(() => {
-                            const cost = watch(
-                              `pricingTiers.${index}.cost` as Path<TFieldValues>,
-                            );
-                            const price = watch(
-                              `pricingTiers.${index}.price` as Path<TFieldValues>,
-                            );
-                            const profit = price - cost;
-                            const profitPercentage =
-                              cost > 0 ? ((profit / cost) * 100).toFixed(0) : 0;
-                            const margin =
-                              price > 0
-                                ? ((profit / price) * 100).toFixed(0)
-                                : 0;
-                            return `M: ${margin}% | P: ${profitPercentage}%`;
-                          })()}
-                        </span>
-                      </div>
+                <td className='whitespace-nowrap px-2 py-2'>
+                  <FormItem>
+                    <FormControl>
+                      <Controller
+                        name={
+                          `pricingTiers.${index}.unitRange.max` as Path<TFieldValues>
+                        }
+                        control={control}
+                        render={({ field }) => (
+                          <input
+                            {...field}
+                            type='number'
+                            className='form-input mx-auto mt-1 block w-20 rounded-none border-b border-gray-300 px-2 py-1 text-center focus:border-indigo-500 focus:outline-none focus:ring-0 sm:text-sm'
+                            onChange={(e) => field.onChange(e.target.value)} // Allow any change
+                            onBlur={() => handleBlur(index)} // Apply validation on blur
+                          />
+                        )}
+                      />
+                    </FormControl>
+                  </FormItem>
+                </td>
 
+                <td className='whitespace-nowrap px-2 py-2'>
+                  <FormItem>
+                    <FormControl>
                       <Controller
                         control={control}
                         name={
-                          `pricingTiers.${index}.price` as Path<TFieldValues>
+                          `pricingTiers.${index}.cost` as Path<TFieldValues>
                         }
                         render={({
                           field: { ref, value, onChange, ...restField },
@@ -267,42 +223,124 @@ const PricingTier = <
                               ref={ref}
                               value={`${currencySymbol}${value}`}
                               onChange={(e) => {
-                                // Ensure we strip out the currency symbol before updating the value
+                                // Allow numbers and decimal point, remove all other characters
                                 const newValue = e.target.value.replace(
-                                  currencySymbol ?? '',
+                                  new RegExp(`[^0-9.]|(?<=\\..*)\\.`, 'g'),
                                   '',
                                 );
-                                onChange(newValue);
+                                // If the newValue starts with a decimal point, prepend a zero
+                                const formattedValue = newValue.startsWith('.')
+                                  ? `0${newValue}`
+                                  : newValue;
+                                onChange(
+                                  formattedValue.replace(
+                                    currencySymbol ?? '',
+                                    '',
+                                  ),
+                                );
                               }}
-                              type='text' // changed to text to handle the currency symbol
                               className='form-input mt-1 block w-full rounded-none border-b border-gray-300 py-1 pl-4 text-center focus:border-indigo-500 focus:outline-none focus:ring-0 sm:text-sm'
                               style={{
-                                paddingLeft: `${currencySymbol?.length ?? '' + 1}ch`,
+                                paddingLeft: `${currencySymbol?.length ?? 1}ch`,
                               }}
                             />
                           </div>
                         )}
                       />
-                    </>
-                  </FormControl>
-                </FormItem>
-              </td>
+                    </FormControl>
+                  </FormItem>
+                </td>
 
-              <td className='px-2 text-sm font-medium'>
-                {index > 0 && (
-                  <button
-                    onClick={() => remove(index)}
-                    className='pt-1 text-red-600 hover:text-red-900 focus:outline-none'
-                  >
-                    <FaXmark />
-                  </button>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+                <td className='relative whitespace-nowrap px-2 py-2'>
+                  <FormItem>
+                    <FormControl>
+                      <>
+                        {/* Absolute positioning for Margin and Profit */}
+                        <div className='absolute -top-1 right-1 flex flex-col items-end'>
+                          <span className='mt-1 text-xs text-blue-500'>
+                            {(() => {
+                              const cost = watch(
+                                `pricingTiers.${index}.cost` as Path<TFieldValues>,
+                              );
+                              const price = watch(
+                                `pricingTiers.${index}.price` as Path<TFieldValues>,
+                              );
+                              const profit = price - cost;
+                              const profitPercentage =
+                                cost > 0
+                                  ? ((profit / cost) * 100).toFixed(0)
+                                  : 0;
+                              const margin =
+                                price > 0
+                                  ? ((profit / price) * 100).toFixed(0)
+                                  : 0;
+                              return `M: ${margin}% | P: ${profitPercentage}%`;
+                            })()}
+                          </span>
+                        </div>
+
+                        <Controller
+                          control={control}
+                          name={
+                            `pricingTiers.${index}.price` as Path<TFieldValues>
+                          }
+                          render={({
+                            field: { ref, value, onChange, ...restField },
+                          }) => (
+                            <div className='relative'>
+                              <input
+                                {...restField}
+                                ref={ref}
+                                value={`${currencySymbol}${value}`}
+                                onChange={(e) => {
+                                  // Remove everything except numbers and the decimal point, and limit to one decimal point
+                                  const newValue = e.target.value.replace(
+                                    new RegExp(`[^0-9.]|(?<=\\..*)\\.`, 'g'),
+                                    '',
+                                  );
+                                  // If the newValue starts with a decimal point, prepend a zero
+                                  const formattedValue = newValue.startsWith(
+                                    '.',
+                                  )
+                                    ? `0${newValue}`
+                                    : newValue;
+                                  // Update the value after removing the currency symbol
+                                  onChange(
+                                    formattedValue.replace(
+                                      currencySymbol ?? '',
+                                      '',
+                                    ),
+                                  );
+                                }}
+                                type='text' // Keep as text to display the currency symbol
+                                className='form-input mt-1 block w-full rounded-none border-b border-gray-300 py-1 pl-4 text-center focus:border-indigo-500 focus:outline-none focus:ring-0 sm:text-sm'
+                                style={{
+                                  paddingLeft: `${currencySymbol?.length ?? 1}ch`,
+                                }}
+                              />
+                            </div>
+                          )}
+                        />
+                      </>
+                    </FormControl>
+                  </FormItem>
+                </td>
+
+                <td className='px-2 text-sm font-medium'>
+                  {index > 0 && (
+                    <button
+                      onClick={() => removeAndUpdate(index)}
+                      className='pt-1 text-red-600 hover:text-red-900 focus:outline-none'
+                    >
+                      <FaXmark />
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
