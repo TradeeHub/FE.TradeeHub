3300c9196330572aadd681f236cd75d48279c1a7
Marenglen Doci <tradeehub@gmail.com>
2024-03-28 20:11:47 +0000

    - removed memo from input

--- app/[locale]/ui/general/SimpleInput/SimpleInput.tsx
+++ app/[locale]/ui/general/SimpleInput/SimpleInput.tsx
@@ -17,111 +17,112 @@ type InputWithIconProps<
   currencySymbol?: string;
 };
 
-const SimpleInput = memo(
-  <TFieldValues extends FieldValues, TName extends FieldPath<TFieldValues>>({
-    field,
-    icon: Icon,
-    autoFocus = false,
-    title = '',
-    placeholder = '',
-    type = 'text',
-    onEnterPress = () => {},
-    currencySymbol
-  }: InputWithIconProps<TFieldValues, TName>) => {
-    const [showPassword, setShowPassword] = useState(false);
-    const [labelFloat, setLabelFloat] = useState(field.value ? true : false);
-    const [isFocused, setIsFocused] = useState(false);
+const SimpleInput = <
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>
+>({
+  field,
+  icon: Icon,
+  autoFocus = false,
+  title = '',
+  placeholder = '',
+  type = 'text',
+  onEnterPress = () => {},
+  currencySymbol
+}: InputWithIconProps<TFieldValues, TName>) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [labelFloat, setLabelFloat] = useState(field.value ? true : false);
+  const [isFocused, setIsFocused] = useState(false);
 
-    const handleToggleShowPassword = useCallback(
-      () => setShowPassword((prev) => !prev),
-      []
-    );
+  const handleToggleShowPassword = useCallback(
+    () => setShowPassword((prev) => !prev),
+    []
+  );
 
-    const handleKeyDown = useCallback(
-      (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter' && onEnterPress) {
-          event.preventDefault();
-          onEnterPress();
-        }
-      },
-      [onEnterPress]
-    );
+  const handleKeyDown = useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === 'Enter' && onEnterPress) {
+        event.preventDefault();
+        onEnterPress();
+      }
+    },
+    [onEnterPress]
+  );
 
-    const handleInputFocus = useCallback(() => {
-      setIsFocused(true);
-      setLabelFloat(true);
-    }, []);
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+    setLabelFloat(true);
+  }, []);
 
-    const handleInputBlur = useCallback(() => {
-      setIsFocused(false);
-      setLabelFloat(!!field.value); // Use !! to convert to boolean ensuring empty strings, null, or undefined set to false
-    }, [field.value]);
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setLabelFloat(!!field.value); // Use !! to convert to boolean ensuring empty strings, null, or undefined set to false
+  }, [field.value]);
 
-    const inputId = `input-${field.name}`;
-    const isPasswordType = type === 'password';
+  const inputId = `input-${field.name}`;
+  const isPasswordType = type === 'password';
 
-    return (
-      <>
-        <div className='relative rounded-md border shadow-sm focus-within:border-primary'>
-          {Icon && (
-            <Icon className='absolute left-3 top-3 h-5 w-5 text-gray-500' />
-          )}
-          {currencySymbol && (
-            <div className='pointer-events-none absolute left-3 top-1/2 flex h-5 w-5 -translate-y-1/2 transform items-center justify-center font-semibold text-primary'>
-              {currencySymbol}
-            </div>
-          )}
-          <input
-            {...field}
-            id={inputId}
-            step={currencySymbol ? '1.00' : undefined}
-            value={field.value === null ? '' : field.value}
-            type={isPasswordType && !showPassword ? 'password' : type}
-            autoFocus={autoFocus}
-            className={`text-md w-full rounded-md border-gray-300 bg-white py-2 focus:border-primary focus:outline-none focus:ring-0 focus:ring-primary ${
-              currencySymbol ? 'pl-8 pr-4' : 'px-4'
-            } ${isPasswordType ? 'pr-12' : ''}`}
-            onKeyDown={handleKeyDown}
-            onFocus={handleInputFocus}
-            onChange={(e) => {
-              const value = e.target.value;
-              // Check if the input is not empty and it's a numeric field
-              if (type === 'number') {
-                const numericValue = value === '' ? '' : Number(value);
-                field.onChange(numericValue);
-              } else {
-                // If the input is empty or not a number, directly pass the value without converting
-                field.onChange(value);
-              }
-            }}
-            onBlur={handleInputBlur}
-          />
-          <label
-            htmlFor={inputId}
-            className={`absolute left-3 bg-white px-1 transition-all duration-200 ease-in-out ${
-              labelFloat || field.value || isFocused
-                ? 'top-[-0.7rem] mb-1 font-roboto text-sm font-semibold text-primary'
-                : 'text-md top-1/2 -translate-y-1/2 text-gray-500'
-            }`}
+  return (
+    <>
+      <div className='relative rounded-md border shadow-sm focus-within:border-primary'>
+        {Icon && (
+          <Icon className='absolute left-3 top-3 h-5 w-5 text-gray-500' />
+        )}
+        {currencySymbol && (
+          <div className='pointer-events-none absolute left-3 top-1/2 flex h-5 w-5 -translate-y-1/2 transform items-center justify-center font-semibold text-primary'>
+            {currencySymbol}
+          </div>
+        )}
+        <input
+          {...field}
+          id={inputId}
+          step={currencySymbol ? '1.00' : undefined}
+          value={field.value === null ? '' : field.value}
+          type={isPasswordType && !showPassword ? 'password' : type}
+          autoFocus={autoFocus}
+          className={`text-md w-full rounded-md border-gray-300 bg-white py-2 focus:border-primary focus:outline-none focus:ring-0 focus:ring-primary ${
+            currencySymbol ? 'pl-8 pr-4' : 'px-4'
+          } ${isPasswordType ? 'pr-12' : ''}`}
+          onKeyDown={handleKeyDown}
+          onFocus={handleInputFocus}
+          onChange={(e) => {
+            const value = e.target.value;
+            // Check if the input is not empty and it's a numeric field
+            if (type === 'number') {
+              const numericValue = value === '' ? '' : Number(value);
+              field.onChange(numericValue);
+            } else {
+              // If the input is empty or not a number, directly pass the value without converting
+              field.onChange(value);
+            }
+          }}
+          onBlur={handleInputBlur}
+        />
+        <label
+          htmlFor={inputId}
+          className={`absolute left-3 bg-white px-1 transition-all duration-200 ease-in-out ${
+            labelFloat || field.value || isFocused
+              ? 'top-[-0.7rem] mb-1 font-roboto text-sm font-semibold text-primary'
+              : 'text-md top-1/2 -translate-y-1/2 text-gray-500'
+          }`}
+        >
+          {labelFloat || field.value || isFocused ? title : placeholder}
+        </label>
+        {isPasswordType && (
+          <button
+            type='button'
+            className='absolute right-3 top-3 cursor-pointer text-xs text-gray-500'
+            onClick={handleToggleShowPassword}
           >
-            {labelFloat || field.value || isFocused ? title : placeholder}
-          </label>
-          {isPasswordType && (
-            <button
-              type='button'
-              className='absolute right-3 top-3 cursor-pointer text-xs text-gray-500'
-              onClick={handleToggleShowPassword}
-            >
-              {showPassword ? 'Hide' : 'Show'}
-            </button>
-          )}
-        </div>
+            {showPassword ? 'Hide' : 'Show'}
+          </button>
+        )}
+      </div>
 
-        <StyledFormMessage />
-      </>
-    );
-  }
-);
+      <StyledFormMessage />
+    </>
+  );
+};
 
 const StyledFormMessage = () => {
   return (
